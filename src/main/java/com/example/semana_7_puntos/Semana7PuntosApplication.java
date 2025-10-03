41c9993949d17e9cc2f5f7f27bcc5fae550afb03
Luciana Pacheco <lucianapacheco@MacBook-Air-de-Luciana.local>
2025-10-02 21:37:37 -0500

    uwu

---
+++ src/main/java/com/example/semana_7_puntos/Semana7PuntosApplication.java
@@ -0,0 +1,13 @@
+package com.example.semana_7_puntos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Semana7PuntosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Semana7PuntosApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/semana_7_puntos/Semana7PuntosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.semana_7_puntos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Semana7PuntosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
